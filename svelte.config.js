87e7588f2a654bac36c34bf58f98cc0541007c26
mrhumble <carlos@kwagei.com>
2021-05-10 16:49:09 +0000

    Add a node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
+// const node = require('@sveltejs/adapter-node');
+import node from '@sveltejs/adapter-node'
+
 const config = {
 	kit: {
+		adapter: node(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
